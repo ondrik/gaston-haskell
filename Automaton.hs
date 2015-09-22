cb348d047e4cbd6b442a0c97855b6e1361fe3774
Ondra Lengal <ondra.lengal@gmail.com>
2015-09-22 08:45:53 +0800

    Automaton.hs: preliminary tests of intersection and subsumption of terms

--- Automaton.hs
+++ Automaton.hs
@@ -60,47 +60,159 @@ toAutomaton = toAutomatonFin
 type State = String
 
 data StateTerm =
-    StateSet [State]
-  | StateTermUnionFin StateTerm StateTerm
-  | StateTermUnionNonfin StateTerm StateTerm
-  | StateTermIsectFin StateTerm StateTerm
-  | StateTermIsectNonfin StateTerm StateTerm
-  | StateTermUpClosed StateTerm
+    STSet [State]
+  | STUnionFin StateTerm StateTerm
+  | STUnionNonfin StateTerm StateTerm
+  | STIsectFin StateTerm StateTerm
+  | STIsectNonfin StateTerm StateTerm
+  | STUpClosed StateTerm
+  | STUpClosedChoice StateTerm
+  | STDownClosed StateTerm
+
 
 -- print out a state term in a human-readable format
-showStateTerm :: StateTerm -> String
-showStateTerm (StateSet xs)                = show xs
-showStateTerm (StateTermUnionFin t1 t2)    = "(" ++ (showStateTerm t1) ++ " ᵤ×ᶠ " ++ (showStateTerm t2) ++ ")"
-showStateTerm (StateTermUnionNonfin t1 t2) = "(" ++ (showStateTerm t1) ++ " ᵤ×ⁿ " ++ (showStateTerm t2) ++ ")"
-showStateTerm (StateTermIsectFin t1 t2)    = "(" ++ (showStateTerm t1) ++ " ᵢ×ᶠ " ++ (showStateTerm t2) ++ ")"
-showStateTerm (StateTermIsectNonfin t1 t2) = "(" ++ (showStateTerm t1) ++ " ᵢ×ⁿ " ++ (showStateTerm t2) ++ ")"
-showStateTerm (StateTermUpClosed t)        = "↑{" ++ showStateTerm t ++ "}"
+showST :: StateTerm -> String
+showST (STSet xs)            = show xs
+showST (STUnionFin t1 t2)    = "(" ++ (showST t1) ++ " ᵤ×ᶠ " ++ (showST t2) ++ ")"
+showST (STUnionNonfin t1 t2) = "(" ++ (showST t1) ++ " ᵤ×ⁿ " ++ (showST t2) ++ ")"
+showST (STIsectFin t1 t2)    = "(" ++ (showST t1) ++ " ᵢ×ᶠ " ++ (showST t2) ++ ")"
+showST (STIsectNonfin t1 t2) = "(" ++ (showST t1) ++ " ᵢ×ⁿ " ++ (showST t2) ++ ")"
+showST (STUpClosed t)        = "↑{" ++ showST t ++ "}"
+showST (STUpClosedChoice t)  = "↑⫫{" ++ showST t ++ "}"
+showST (STDownClosed t)      = "↓{" ++ showST t ++ "}"
 
 -- instantiance of the data type as class Show
 instance Show StateTerm where
-  show = showStateTerm
+  show = showST
 
 
 -- initial states
 initial :: Aut -> StateTerm
 initial (AutAtomicFin phi)
-  | phi == "X ⊆ Y"    = StateSet ["q1"]
-  | phi == "X ⊇ Y"    = StateSet ["q2"]
-  | phi == "Z = σ(Y)" = StateSet ["q3"]
-  | otherwise         = error "Unknown atomic predicate"
+  | phi == "X ⊆ Y"    = STSet ["q1"]
+  | phi == "X ⊇ Y"    = STSet ["q2"]
+  | phi == "Z = σ(Y)" = STSet ["q3"]
+  | otherwise         = error "initial: Unknown atomic predicate"
 initial (AutAtomicNonfin phi)
-  | phi == "X ⊆ Y"    = StateSet ["q1"]
-  | phi == "X ⊇ Y"    = StateSet ["q2"]
-  | phi == "Z = σ(Y)" = StateSet ["q3"]
-  | otherwise         = error "Unknown atomic predicate"
-initial (AutUnionFin a1 a2)    = (initial a1) `StateTermUnionFin` (initial a2)
-initial (AutUnionNonfin a1 a2) = (initial a1) `StateTermUnionNonfin` (initial a2)
-initial (AutIsectFin a1 a2)    = (initial a1) `StateTermIsectFin` (initial a2)
-initial (AutIsectNonfin a1 a2) = (initial a1) `StateTermIsectNonfin` (initial a2)
-initial (AutComplFin a)        = StateTermUpClosed $ initial a
-initial (AutComplNonfin a)     = StateTermUpClosed $ initial a
+  | phi == "X ⊆ Y"    = STSet ["q1"]
+  | phi == "X ⊇ Y"    = STSet ["q2"]
+  | phi == "Z = σ(Y)" = STSet ["q3"]
+  | otherwise         = error "initial: Unknown atomic predicate"
+initial (AutUnionFin a1 a2)    = (initial a1) `STUnionFin` (initial a2)
+initial (AutUnionNonfin a1 a2) = (initial a1) `STUnionNonfin` (initial a2)
+initial (AutIsectFin a1 a2)    = (initial a1) `STIsectFin` (initial a2)
+initial (AutIsectNonfin a1 a2) = (initial a1) `STIsectNonfin` (initial a2)
+initial (AutComplFin a)        = STUpClosed $ initial a
+initial (AutComplNonfin a)     = STUpClosed $ initial a
 initial (AutProjFin var a)     = initial a
 initial (AutProjNonfin var a)  = initial a
+
+
+-- final states
+final :: Aut -> StateTerm
+final (AutAtomicFin phi)
+  | phi == "X ⊆ Y"    = STSet ["q1"]
+  | phi == "X ⊇ Y"    = STSet ["q2"]
+  | phi == "Z = σ(Y)" = STSet ["q3"]
+  | otherwise         = error "final: Unknown atomic predicate"
+final (AutUnionFin a1 a2)    = (final a1) `STUnionFin` (final a2)
+final (AutIsectFin a1 a2)    = (final a1) `STIsectFin` (final a2)
+final (AutComplFin a)        = STDownClosed $ nonfinal a
+final (AutProjFin var a)     = final a           -- FIXME: this is clearly wrong
+
+
+-- nonfinal states
+nonfinal :: Aut -> StateTerm
+nonfinal (AutAtomicNonfin phi)
+  | phi == "X ⊆ Y"    = STSet []
+  | phi == "X ⊇ Y"    = STSet []
+  | phi == "Z = σ(Y)" = STSet ["q4"]
+  | otherwise         = error "nonfinal: Unknown atomic predicate"
+nonfinal (AutUnionNonfin a1 a2) = (nonfinal a1) `STUnionNonfin` (nonfinal a2)
+nonfinal (AutIsectNonfin a1 a2) = (nonfinal a1) `STIsectNonfin` (nonfinal a2)
+nonfinal (AutComplNonfin a)     = STUpClosedChoice $ final a
+nonfinal (AutProjNonfin var a)  = nonfinal a     -- FIXME: this is clearly wrong
+
+
+-- zero-predecessors of a state term with a transition function with given variables projected out
+pre :: Aut -> [Var] -> StateTerm -> StateTerm
+pre (AutUnionFin a1 a2) vars (STUnionFin t1 t2) = (pre a1 vars t1) `STUnionFin` (pre a2 vars t2)
+pre (AutIsectFin a1 a2) vars (STIsectFin t1 t2) = (pre a1 vars t1) `STIsectFin` (pre a2 vars t2)
+pre (AutComplFin a) vars (STDownClosed t)       = STDownClosed (cpre a vars t)
+pre (AutProjFin var a) vars t                   = pre a vars t        -- FIXME: this is clearly wrong
+pre (AutAtomicFin phi) vars t
+  | phi == "X ⊆ Y"    = error $ "pre(X ⊆ Y); vars = " ++ (show vars) ++ "; t = " ++ (show t)
+  | phi == "X ⊇ Y"    = error $ "pre(X ⊇ Y)" ++ (show vars) ++ "; t = " ++ (show t)
+  | phi == "Z = σ(Y)" = error $ "pre(Z = σ(Y))" ++ (show vars) ++ "; t = " ++ (show t)
+  | otherwise         = error "pre: Unknown atomic predicate"
+pre aut vars t =
+  error $ "Invalid input of pre: aut = " ++ (show aut) ++ "; vars = " ++ (show vars) ++ "; t = " ++ (show t)
+
+
+-- zero-(controllable) predecessors of a state term with a transition function with given variables projected out
+cpre :: Aut -> [Var] -> StateTerm -> StateTerm
+cpre (AutUnionNonfin a1 a2) vars (STUnionNonfin t1 t2) = (cpre a1 vars t1) `STUnionNonfin` (cpre a2 vars t2)
+cpre (AutIsectNonfin a1 a2) vars (STIsectNonfin t1 t2) = (cpre a1 vars t1) `STIsectNonfin` (cpre a2 vars t2)
+cpre (AutComplNonfin a) vars (STUpClosedChoice t)      = STUpClosedChoice (pre a vars t)
+cpre (AutProjNonfin var a) vars t                      = cpre a vars t        -- FIXME: this is clearly wrong
+cpre (AutAtomicNonfin phi) vars t
+  | phi == "X ⊆ Y"    = error $ "cpre(X ⊆ Y)" ++ (show vars) ++ "; t = " ++ (show t)
+  | phi == "X ⊇ Y"    = error $ "cpre(X ⊇ Y)" ++ (show vars) ++ "; t = " ++ (show t)
+  | phi == "Z = σ(Y)" = error $ "cpre(Z = σ(Y))" ++ (show vars) ++ "; t = " ++ (show t)
+  | otherwise         = error "cpre: Unknown atomic predicate"
+cpre aut vars t =
+  error $ "Invalid input of cpre: aut = " ++ (show aut) ++ "; vars = " ++ (show vars) ++ "; t = " ++ (show t)
+
+
+-- used as a return value of evaluation functions
+type ReturnVal = (Bool, StateTerm)
+
+
+-- checks whether the intersection of a pair of lists is nonempty
+listIsectNonempty :: Eq a => [a] -> [a] -> Bool
+listIsectNonempty lhs rhs = or $ map (\x -> x `elem` rhs) lhs
+
+
+-- tests whether an intersection of state terms is nonempty
+isectNonempty :: Aut -> StateTerm -> StateTerm -> ReturnVal
+isectNonempty (AutUnionFin a1 a2) (STUnionFin l1 l2) (STUnionFin r1 r2) = ((fst res1) || (fst res2), (snd res1) `STUnionFin` (snd res2))
+  where
+    res1 = isectNonempty a1 l1 r1
+    res2 = isectNonempty a2 l2 r2
+isectNonempty (AutIsectFin a1 a2) (STIsectFin l1 l2) (STIsectFin r1 r2) = ((fst res1) && (fst res2), (snd res1) `STIsectFin` (snd res2))
+  where
+    res1 = isectNonempty a1 l1 r1
+    res2 = isectNonempty a2 l2 r2
+isectNonempty (AutComplFin a) (STUpClosed lhs) (STDownClosed rhs)       = (fst res, STDownClosed (snd res))
+  where
+    res = isSubset a lhs rhs
+isectNonempty (AutProjFin var a) lhs rhs                                = isectNonempty a lhs rhs        -- FIXME: this is wrong
+isectNonempty (AutAtomicFin _) (STSet lhs) (STSet rhs)                  = (listIsectNonempty lhs rhs, STSet rhs)
+isectNonempty aut lhs rhs =
+  error $ "isectNonempty: incompatible terms in aut = " ++ (show aut) ++ "; lhs = " ++ (show lhs) ++ "; rhs = " ++ (show rhs)
+
+-- checks whether one list is a subset of another list
+listIsSubset :: Eq a => [a] -> [a] -> Bool
+listIsSubset lhs rhs = and $ map (\x -> x `elem` rhs) lhs
+
+
+-- tests whether one state term is (semantically) a subset of another one
+isSubset :: Aut -> StateTerm -> StateTerm -> ReturnVal
+isSubset (AutUnionNonfin a1 a2) (STUnionNonfin l1 l2) (STUnionNonfin r1 r2) = ((fst res1) || (fst res2), (snd res1) `STUnionNonfin` (snd res2))
+  where
+    res1 = isSubset a1 l1 r1
+    res2 = isSubset a2 l2 r2
+isSubset (AutIsectNonfin a1 a2) (STIsectNonfin l1 l2) (STIsectNonfin r1 r2) = ((fst res1) && (fst res2), (snd res1) `STIsectNonfin` (snd res2))
+  where
+    res1 = isSubset a1 l1 r1
+    res2 = isSubset a2 l2 r2
+isSubset (AutComplNonfin a) (STUpClosed lhs) (STUpClosedChoice rhs)         = (fst res, STUpClosedChoice (snd res))
+  where
+    res = isectNonempty a lhs rhs
+isSubset (AutProjNonfin var a) lhs rhs                                      = isSubset a lhs rhs        -- FIXME: this is wrong
+isSubset (AutAtomicNonfin _) (STSet lhs) (STSet rhs)                        = (listIsSubset lhs rhs, STSet rhs)
+isSubset aut lhs rhs =
+  error $ "isSubset: incompatible terms in aut = " ++ (show aut) ++ "; lhs = " ++ (show lhs) ++ "; rhs = " ++ (show rhs)
 
 
 -- the automaton for the example formula
